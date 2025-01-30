986a284b85bd4a95707889f26576cce15eefcf28
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-30 09:06:28 +0100

    Started reworking code with using Promise.all instead. Saved -30 days in variable and also got all the movies that got reviews.

--- src/top5Movies.js
+++ src/top5Movies.js
@@ -1,40 +1,22 @@
 import { loadReviews, loadMovies } from './movies.js';
 
-//////////////////
-//THIS CODE KINDA WORKS BUT CAN BE IMPROVED!
-/////////////////
-
 export async function top5Movies() {
   const movies = await loadMovies();
-  const moviesWithRating = [];
 
-  for (const movie of movies) {
-    const reviews = await loadReviews(movie.id);
+  const thirtyDaysAgo = new Date();
+  thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+  console.log(thirtyDaysAgo);
 
-    if (reviews.length === 0) continue;
-
-    const validReviews = reviews.filter((r) => r.attributes.rating !== null);
-    if (validReviews.length === 0) continue;
-
-    let totalRating = 0;
-    for (const review of validReviews) {
-      totalRating += review.attributes.rating;
-    }
-    const averageRating = totalRating / validReviews.length;
-
-    moviesWithRating.push({
-      ...movie,
-      reviews: validReviews,
-      averageRating,
-    });
-  }
-  const topMovies = moviesWithRating.sort((a, b) => b.averageRating - a.averageRating).slice(0, 5);
-
-  console.log(
-    '---------TOP 5 MOVIES:',
-    topMovies.map((movie) => ({
-      title: movie.title,
-      averageRating: movie.averageRating,
-    }))
+  const moviesWithReviews = await Promise.all(
+    movies.map(async (movie) => {
+      const reviews = await loadReviews(movie.id);
+      return {
+        ...movie,
+        reviews,
+      };
+    })
   );
+
+  const moviesWithRatings = moviesWithReviews;
+  console.log(moviesWithRatings);
 }
