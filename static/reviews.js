afa80efc7bb0b536e04528f0eb1e21500277253a
Johannes <johannes.persson@lerniastudent.se>
2025-01-31 16:07:21 +0100

    start to add function to buttons

--- static/reviews.js
+++ static/reviews.js
@@ -1,15 +1,14 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-  async function fetchReviews (page) {
+    async function fetchReviews (page) {
       const url = (`/api/reviews/${movieId}?page=${page}`);
-
-    const res = await fetch(url)
-    const data = await res.json();
-
+      const res = await fetch(url)
+      const data = await res.json();
+   
   const revUl = document.querySelector('.review-ul');
   
-  console.log('Recensioner: ', data);
-  
+  revUl.innerHTML = "";
+
   data.reviews.forEach(review => {
       const author = review.author;
       const rating = review.rating;
@@ -24,9 +23,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
       revUl.appendChild(revCom);
   });
+
+  // prevButton.disable = data.pagignation.page === 1;
+  // nextButton.disable = data.pagignation.page >= data.pagignation.pageCount;
+  return data;
 }
 
-fetchReviews();
+const data = fetchReviews(); 
+console.log(data); 
+
+let currentPage = 1;
+const prevButton = document.querySelector('.review-prev');
+prevButton.addEventListener("click", () =>{
+if (currentPage > 1) {
+  currentPage--;
+  fetchReviews(currentPage)
+}
+});
+
+const nextButton = document.querySelector('.review-next');
+nextButton.addEventListener('click', () =>{
+  if((currentPage * 5) < 15){
+    currentPage++;
+  fetchReviews(currentPage);
+  }
+  
+});
+
+fetchReviews(currentPage);
 });
 
 
