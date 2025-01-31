a551bf53769e47fbfa1682f8018d830761e68fb3
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-31 09:31:35 +0100

    switching branch

--- src/top5Movies.js
+++ src/top5Movies.js
@@ -30,32 +30,33 @@ export async function top5Movies() {
     .map((movie) => {
       //filter out not valid reviews, has to have rating > 0 and be withing the date.
       const validReviews = movie.reviews.filter((review) => {
-        const reviewDate = new Date(review.attributes.createdAt);
-        return review.attributes.rating !== null && reviewDate >= thirtyDaysAgo;
+        const reviewDate = new Date(review.attributes.createdAt); //Makes the date to a date object
+        return review.attributes.rating !== null && reviewDate >= thirtyDaysAgo; //Only reviews from the latest 30 days with reviews are used
       });
       // Count average rating if there are valid reviews.
       const averageRating =
         validReviews.length > 0
           ? validReviews.reduce((sum, review) => sum + review.attributes.rating, 0) / validReviews.length
-          : null;
-      //returns movie with its rating and number of reviews.
+          : null; //IF there are no valid reviews, the rating is set to null.
+
+      //returns movie with its average review rating & number of valid reviews
       return {
-        ...movie,
-        averageRating,
-        reviewCount: validReviews.length,
+        ...movie, //Keeps all movie data
+        averageRating, //adds the average rating
+        reviewCount: validReviews.length, //number of valid reviews
       };
     })
     .filter((movie) => movie.averageRating !== null); //removes movies that does not have valid rating.
 
-  //Sorts movies and choose the top 5.
+  //Sorts movies and choose the top 5 based on the best rating and number of reviews.
   const topMovies = moviesWithRating
     .sort((a, b) => {
       if (b.averageRating !== a.averageRating) {
-        return b.averageRating - a.averageRating;
+        return b.averageRating - a.averageRating; //sorts first based on best average rating.
       }
-      return b.reviewCount - a.reviewCount;
+      return b.reviewCount - a.reviewCount; //If a movie has the same rating, it gets sorted based on most number of reviews.
     })
-    .slice(0, 5);
+    .slice(0, 5); //takes the tyop 5 movies.
 
   console.log('\n=== SLUTRESULTAT ===');
   console.log('Topp 5 filmer (senaste 30 dagarna):');
