cf7b62d4c5bca0e59568badcadcdbe3f738bfb2b
Louise <emmalouisekarlsson@hotmail.com>
2025-02-04 00:18:39 +0100

    adjusted fakeReview

--- tests/reviews.test.js
+++ tests/reviews.test.js
@@ -3,22 +3,26 @@ import fetchReviews from '../static/loadReviews.js';
 import loadReviews from '../static/reviews.js';
 
 const fakeReview = {
-  reviews: {
-    author: "Kalle",
-    rating: 3,
-    comment: "Hej"
-  },
-  pagination: {
-    page: 1,
-    pageCount: 5,
-    pageSize: 5,
-    total: 25
+  reviews: [
+    {
+      author: "Kalle",
+      rating: 3,
+      comment: "Hej"
+    }
+  ],
+  meta: {
+    pagination: {
+      page: 1,
+      pageSize: 5,
+      pageCount: 5,
+      total: 24
+    }
   }
 }
 
 test('Should display page 1', () => {
-  document.addEventListener('DOMContentLoaded', function () {
-    fetchReviews();
+  document.addEventListener('DOMContentLoaded', async function () {
+    await fetchReviews();
     const review = new loadReviews(fakeReview);
     review.render(document);
 
@@ -27,8 +31,8 @@ test('Should display page 1', () => {
 });
 
 test('Should display page 2', () => {
-  document.addEventListener('DOMContentLoaded', function () {
-    fetchReviews(2);
+  document.addEventListener('DOMContentLoaded', async function () {
+    await fetchReviews(2)
     const review = new loadReviews(fakeReview);
     review.render(document);
 
@@ -37,8 +41,8 @@ test('Should display page 2', () => {
 });
 
 test('Next button should be disabel', () => {
-  document.addEventListener('DOMContentLoaded', function () {
-    fetchReviews(6);
+  document.addEventListener('DOMContentLoaded', async function () {
+    await fetchReviews(6);
     const review = new loadReviews(fakeReview);
     review.render(document);
 
