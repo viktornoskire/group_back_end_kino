23e3ed67854b1e11e62226f28694712ed761fe58
Louise <emmalouisekarlsson@hotmail.com>
2025-02-04 07:38:19 +0100

    reviews

--- static/reviews.js
+++ static/reviews.js
@@ -1,56 +1,12 @@
-import fetchReviews from "./loadReviews.js";
+import fetchReviews from "./fetchReviews.js";
+import loadReviews from "./loadReviews.js";
 
-document.addEventListener('DOMContentLoaded', function () {
-  fetchReviews();
-});
+export default async function getReviews(page) {
+  const data = await fetchReviews(page);
+  console.log("data", data);
 
-export default class loadReviews {
-  constructor(reviews) {
-    this.reviews = reviews;
-  }
-
-  render(elem) {
-    const revUl = elem.querySelector('.review-ul');
-
-    revUl.innerHTML = "";
-
-    this.reviews.reviews.forEach(review => {
-      const author = review.author;
-      const rating = review.rating;
-      const comment = review.comment;
-
-      const revCom = elem.createElement('li');
-      revCom.classList.add('review-li')
-      revCom.innerHTML = `
-    <p><strong>${author}</strong><p>
-    <p>Betyg - ${rating}</p>
-    <p>${comment}</p>`;
-
-      revUl.appendChild(revCom);
-    });
-
-    let currentPage = this.reviews.pagination.page;
-
-    const prevButton = elem.querySelector('.review-prev');
-    prevButton.addEventListener("click", () => {
-      if (currentPage > 1) {
-        currentPage--;
-        fetchReviews(currentPage)
-        nextButton.disabled = false;
-      } else {
-        prevButton.disabled = true;
-      }
-    });
-
-    const nextButton = elem.querySelector('.review-next');
-    nextButton.addEventListener('click', () => {
-      if ((currentPage * 5) < this.reviews.pagination.total) {
-        currentPage++;
-        fetchReviews(currentPage);
-        prevButton.disabled = false;
-      } else {
-        nextButton.disabled = true;
-      }
-    });
-  }
+  const dataReviews = new loadReviews(data);
+  dataReviews.render(document);
 }
+
+getReviews(); 
