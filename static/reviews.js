07279274d67f2cebb8c511afebb27e49e6c038a8
Louise <emmalouisekarlsson@hotmail.com>
2025-02-04 21:45:48 +0100

    Fetch ont frontend and load page

--- static/reviews.js
+++ static/reviews.js
@@ -1,13 +1,75 @@
-import cmsAdapter from "./fetchReviews.js";
-import loadReviews from "./loadReviews.js";
+async function reviews(page) {
 
-export default async function getReviews(cmsAdapter, page) {
-  const data = await cmsAdapter.fetchReviews(page);
+  const parts = window.location.pathname.split('/');
+  const movieId = parts.length > 2 ? parts[2] : null;
 
-  const dataReviews = new loadReviews(data);
-  dataReviews.render(document);
+  page = page || 1; //Needs to be declared 
 
+  try {
+    const response = await fetch(`/api/reviews/${movieId}/${page}`);
+    const data = await response.json();
+
+    console.log("min data", data);
+
+    const revUl = document.querySelector('.review-ul');
+    revUl.innerHTML = "";
+
+    data.reviews.forEach(review => {
+      const revLi = document.createElement('li');
+      revLi.classList.add('review-li');
+      revUl.append(revLi);
+
+      const revAut = document.createElement('p')
+      revAut.textContent = review.author;
+      revLi.append(revAut);
+
+      const revRat = document.createElement('p')
+      revRat.textContent = review.rating;
+      revLi.append(revRat);
+
+      const revCom = document.createElement('p')
+      revCom.textContent = review.comment;
+      revLi.append(revCom);
+
+    });
+
+    let currentPage = data.pagination.page;
+
+    const prevButton = document.querySelector('.review-prev');
+    if (currentPage == 1) {
+      prevButton.disabled = true;
+    }
+
+    prevButton.addEventListener("click", () => {
+      if (currentPage > 1) {
+        currentPage--;
+        reviews(currentPage);
+        nextButton.disabled = false;
+      } else {
+        prevButton.disabled = true;
+      }
+    });
+
+    const nextButton = document.querySelector('.review-next');
+
+    if (currentPage == data.pagination.pageCount) {
+      nextButton.disabled = true;
+    }
+
+    nextButton.addEventListener('click', () => {
+      if ((currentPage * 5) < data.pagination.total) {
+        currentPage++;
+        reviews(currentPage);
+        prevButton.disabled = false;
+      } else {
+        nextButton.disabled = true;
+      }
+    });
+
+  } catch (error) {
+    console.log('Couldnt get reviwes', error);
+  }
 }
 
-getReviews(cmsAdapter);
+reviews();
 
