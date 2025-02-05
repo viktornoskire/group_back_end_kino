ad65dc9c6a8783075928bf71a014ac213a693d3b
Louise <emmalouisekarlsson@hotmail.com>
2025-02-05 08:52:22 +0100

    Grundstructor for test

--- tests/reviews.test.js
+++ tests/reviews.test.js
@@ -1,31 +1,40 @@
 import { describe, test, expect } from '@jest/globals';
-import { reviews } from '../static/reviews';
+import { getReviews } from '../static/loadReviews.js';
 
 describe('Paginated review', () => {
 
   test('Should display page 1', async () => {
-    const cmsReviews = {
-      fetchReviews: async () => [
-        mockReview({
-          page: 1,
-        }),
-      ]
-    }
 
-    //Test here
+    const mockReviews = {
+      fetchReviews: async () => {
+        return {
+          reviews: [mockReview()],
+          pagination: {
+            meta: {
+              page: 1,
+            }
+          }
+        };
+      }
+    };
+
+    const rev = await getReviews(mockReviews);
+
+    expect(rev.pagination.meta.page).toBe(1);
 
   });
-
 });
 
 function mockReview(overrids) {
   return {
     pagination: {
-      page: 1,
-      pageSize: 5,
-      pageCount: 5,
-      total: 24,
-      ...overrids
+      meta: {
+        page: 1,
+        pageSize: 5,
+        pageCount: 5,
+        total: 24,
+        ...overrids
+      }
     },
-  }
+  };
 }
