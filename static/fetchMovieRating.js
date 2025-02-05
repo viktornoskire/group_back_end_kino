3d7d016c8cc3d5744790ccd89691accfb69c01eb
ViktorNoskire <vikkerinho@gmail.com>
2025-02-05 17:10:05 +0100

    Render the rating on movie page

--- static/fetchMovieRating.js
+++ static/fetchMovieRating.js
@@ -2,7 +2,22 @@
   if (document.querySelector('.movie-title')) {
     const movieId = window.location.pathname.split('/')[2];
     const res = await fetch(`/api/screenings/${movieId}`);
-    const {rating} = await res.json();
-    document.querySelector('.rating-rating').innerText = Math.round(rating);
+    const { rating } = await res.json();
+
+    const titleDiv = document.querySelector(".title-div");
+    const ratingEl = document.createElement("p");
+    ratingEl.classList.add("rating-title");
+    ratingEl.innerText = "Rating: ";
+
+    const star = document.createElement("i");
+    star.setAttribute("class", "fa-solid fa-star")
+
+    const ratingNum = document.createElement("p");
+    ratingNum.classList.add("rating-num");
+    ratingNum.innerText = Math.round(rating * 10) / 10;
+
+    titleDiv.appendChild(ratingEl);
+    titleDiv.appendChild(star);
+    titleDiv.appendChild(ratingNum);
   }
 })();
