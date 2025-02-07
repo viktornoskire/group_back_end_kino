7d72ffc657cd079b9a6c6336d71b4c049de7afd1
ViktorNoskire <vikkerinho@gmail.com>
2025-02-07 16:10:22 +0100

    Complete the functionality for accessing the protected api

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { getMovieRating } from './rating.js';
 import cmsAdapterRating from './cmsAdapterRating.js';
 import cmsAdapter from './cmsAdapterTop5Movies.js';
 import cmsReviews from '../static/cmsReviews.js';
+import jsonwebtoken from 'jsonwebtoken';
 
 export default function initialize(api) {
   const app = express();
@@ -116,6 +117,96 @@ export default function initialize(api) {
     } catch (e) {
       console.error(`Problems with fetching the screenings, ${e}`);
       res.status(500).json({ message: 'Could not fetch any screenings' });
+    }
+  });
+
+  const LOGIN = [
+    {
+      username: 'viktor',
+      password: 'wilma',
+    },
+    {
+      username: 'rikard',
+      password: 'rikard3209',
+    },
+    {
+      username: 'johnny',
+      password: 'zidust',
+    },
+    {
+      username: 'johannes',
+      password: 'spinalglitter38',
+    },
+    {
+      username: 'louise',
+      password: 'lillamylouise',
+    },
+    {
+      username: 'emily',
+      password: 'emily_92251',
+    },
+    {
+      username: 'oscar',
+      password: 'oscar0709',
+    },
+  ];
+  const USERNAME = "admin";
+  const PASSWORD = "secret";
+  const SECRET = "averylongpasswordthattheonlyonewhoknowswhatitisisthiscomputercodeblock";
+
+  app.post("/api/login", (req, res) => {
+    const authHeader = req.headers.authorization;
+    const b64credentials = authHeader.slice(6);
+    const credentials = atob(b64credentials);
+    const fields = credentials.split(":");
+    const username = fields[0];
+    const password = fields[1];
+    console.log("Username:", username);
+    console.log("Password:", password);
+
+    if (
+      (
+        username == LOGIN[0].username && password == LOGIN[0].password ||
+        username == LOGIN[1].username && password == LOGIN[1].password ||
+        username == LOGIN[2].username && password == LOGIN[2].password ||
+        username == LOGIN[3].username && password == LOGIN[3].password ||
+        username == LOGIN[4].username && password == LOGIN[4].password ||
+        username == LOGIN[5].username && password == LOGIN[5].password ||
+        username == LOGIN[6].username && password == LOGIN[6].password 
+      )
+    ) {
+      const jwt = jsonwebtoken.sign(
+        {
+          username: username,
+          role: 'reviewer',
+        },
+        SECRET
+      );
+
+      res.status(200).json({
+        ok: true,
+        token: jwt,
+      });
+    } else {
+      res.status(401).end();
+    }
+  });
+
+  app.get("/api/protected", (req, res) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader?.slice(7);
+
+    try {
+      const payload = jsonwebtoken.verify(token, SECRET);
+      res.status(200).json({
+        hello: payload.username,
+        secretData: "this is very secret",
+      });
+    } catch (error) {
+      res.status(401).json({
+        ok: false,
+        error: "not allowed",
+      });
     }
   });
 
--- static/eventListeners.js
+++ static/eventListeners.js
@@ -45,60 +45,92 @@ t.addEventListener('click', () => {
 // __Send review inputs to swagger API__________________________
 if (document.querySelector('.movie-title')) {
   const reviewForm = document.querySelector('.review-box');
-  const login = document.querySelector(".form-container");
-  const overlay = document.querySelector(".blur");
+  const login = document.querySelector('.form-container');
+  const overlay = document.querySelector('.blur');
+  const submitLogin = document.querySelector('.login-submit');
   const API_URL = 'https://plankton-app-xhkom.ondigitalocean.app/api/';
 
+  // reviewForm.addEventListener('submit', async (e) => {
+  //   login.classList.add('active');
+  //   e.preventDefault();
+
+  //   const comment = reviewForm.querySelector('.review-input');
+  //   const rating = reviewForm.querySelector('.rating-input');
+  //   const name = reviewForm.querySelector('.name-input');
+  //   const error = reviewForm.querySelector('.error-message');
+
+  //   error.style.display = 'none';
+
+  //   const movieID = window.location.pathname.split('/')[2];
+
+  //   if (comment.value == '' || name.value == '') {
+  //     console.log('Movie ID: ', movieID);
+  //     console.log('Comment: ', comment.value ? comment.value : 'No comment inserted');
+  //     console.log('Rating: ', rating.value);
+  //     console.log('Name: ', name.value ? name.value : 'No name inserted');
+  //     error.style.display = 'inline';
+  //   } else {
+  //     console.log('Movie ID: ', movieID);
+  //     console.log('Comment: ', comment.value);
+  //     console.log('Rating: ', rating.value);
+  //     console.log('Name: ', name.value);
+  //     fetch(API_URL + 'reviews', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({
+  //         data: {
+  //           comment: comment.value,
+  //           rating: rating.value,
+  //           author: name.value,
+  //           movie: movieID,
+  //           verified: true,
+  //         },
+  //       }),
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => console.log('Success:', data))
+  //       .catch((error) => console.log('Error:', error));
+  //   }
+  //   comment.value = '';
+  //   rating.value = 0;
+  //   name.value = '';
+  // });
+
   reviewForm.addEventListener('submit', async (e) => {
-    login.classList.add('active');
     e.preventDefault();
+    login.classList.add('active');
+    overlay.classList.add('active');
 
-    const comment = reviewForm.querySelector('.review-input');
-    const rating = reviewForm.querySelector('.rating-input');
-    const name = reviewForm.querySelector('.name-input');
-    const error = reviewForm.querySelector('.error-message');
+    submitLogin.addEventListener('click', async () => {
+      const username = document.querySelector("input[name='username']");
+      const password = document.querySelector("input[name='password']");
+      const credentials = `${username.value}:${password.value}`;
+      const b64credentials = btoa(credentials);
 
-    error.style.display = 'none';
+      console.log(b64credentials);
 
-    const movieID = window.location.pathname.split("/")[2];
-
-    if (comment.value == '' || name.value == '') {
-      console.log('Movie ID: ', movieID);
-      console.log('Comment: ', comment.value ? comment.value : 'No comment inserted');
-      console.log('Rating: ', rating.value);
-      console.log('Name: ', name.value ? name.value : 'No name inserted');
-      error.style.display = 'inline';
-    } else {
-      console.log('Movie ID: ', movieID);
-      console.log('Comment: ', comment.value);
-      console.log('Rating: ', rating.value);
-      console.log('Name: ', name.value);
-      fetch(API_URL + "reviews", {
-        method: 'POST',
+      const loginRes = await fetch("/api/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Authorization": "Basic " + b64credentials,
         },
-        body: JSON.stringify({
-          data: {
-            comment: comment.value,
-            rating: rating.value,
-            author: name.value,
-            movie: movieID,
-            verified: true,
-          },
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => console.log('Success:', data))
-        .catch((error) => console.log('Error:', error));
-    }
-    comment.value = '';
-    rating.value = 0;
-    name.value = '';
-  });
+      });
+      const loginPayload = await loginRes.json();
 
-  reviewForm.addEventListener("submit", async () => {
-    login.classList.add("active");
-    overlay.classList.add("active");
-  })
+      const res = await fetch("/api/protected", {
+        headers: {
+          "Authorization": "Bearer " + loginPayload.token,
+        },
+      });
+      const payload = await res.json();
+      console.log(payload);
+
+      username.value = '';
+      password.value = '';
+      login.classList.remove('active');
+      overlay.classList.remove('active');
+    });
+  });
 }
